e3b242fd0f704a80a4afadfe0ce66c5cd6bf6ec2
Ms-27 <ms@juliver.com>
2020-01-10 16:14:55 +0100

    Avancée exos

--- ex193_test.py
+++ ex193_test.py
@@ -5,10 +5,11 @@ from selenium.webdriver.common.alert import Alert
 def test_alert():
     driver =webdriver.Chrome()
     driver.get('https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_alert')
-    modal = driver.find_element_by_id('iframeResult')
-    driver.switch_to.frame(modal)
-    button = modal.find_element_by_tag_name('button')
+    frame = driver.find_element_by_id('iframeResult')
+    driver.switch_to.frame(frame)
+    button = driver.find_element_by_tag_name('button')
     button.click()
-    assert Alert.text == 'Hello! I am an alert box!'
-    Alert.accept()
+    alert = driver.switch_to.alert
+    assert alert.text == 'Hello! I am an alert box!'
+    alert.accept()
     
